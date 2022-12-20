a20f570b1d830edf9489ce07cb1e476889f0e00c
Pninja12 <74876860+Pninja12@users.noreply.github.com>
2022-12-20 14:41:46 +0000

    O começo
    O jogo inicia

--- Game.py
+++ Game.py
@@ -9,9 +9,19 @@ fontname = pygame.font.Font('arial.ttf',40)
 exit = myfont.render('EXIT' , True , (255,255,255))
 start = myfont.render('START' , True , (255,255,255))
 name = fontname.render('Pew-Pew-Comets' , True , (255,255,255))
+comeco = 0
+red = [200,50,50]
+white = [255,255,255]
+playerx = 100
+playery = 100
 
 screen = pygame.display.set_mode((800, 600)) 
 pygame.display.set_caption("COMETS") 
+
+def player(x,y):
+    pygame.draw.line(screen,red, (x - 25,y + 25), (x,y - 25),2)
+    pygame.draw.line(screen,red, (x,y - 25), (x + 25,y + 25),2)
+    pygame.draw.line(screen,white, (x + 25,y + 25), (x - 25,y + 25),2)
 
 keys=pygame.key.get_pressed()
 while True:
@@ -27,25 +37,28 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN and (300 <= rato[0] <= 400 and 200 <= rato[1] <= 250):
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN and (300 <= rato[0] <= 400 and 150 <= rato[1] <= 200):
+            comeco = 1
     
     rato = pygame.mouse.get_pos()
-    
-    while(True):
-        pygame.draw.rect(screen,(0,0,0),[300,150,100,40])
-        pygame.draw.rect(screen,(0,0,0),[300,200,100,40])
-        if 300 <= rato[0] <= 400 and 150 <= rato[1] <= 200:
-            pygame.draw.rect(screen,(255,0,0),[300,150,150,40])
+    if comeco == 0:
+        while(True):
+            pygame.draw.rect(screen,(0,0,0),[300,150,100,40])
+            pygame.draw.rect(screen,(0,0,0),[300,200,100,40])
+            if 300 <= rato[0] <= 400 and 150 <= rato[1] <= 200:
+                pygame.draw.rect(screen,(255,0,0),[300,150,150,40])
 
 
-        elif 300 <= rato[0] <= 400 and 200 <= rato[1] <= 250:
-            pygame.draw.rect(screen,(255,0,0),[300,200,150,40])
+            elif 300 <= rato[0] <= 400 and 200 <= rato[1] <= 250:
+                pygame.draw.rect(screen,(255,0,0),[300,200,150,40])
 
-        screen.blit(start , (300,150))
-        screen.blit(exit , (300,200))
-        screen.blit(name , (200,100))
-        break
+            screen.blit(start , (300,150))
+            screen.blit(exit , (300,200))
+            screen.blit(name , (200,100))
+            break
+    else:
+        player(playerx, playery)
     pygame.display.update()
-    
 
 
 
